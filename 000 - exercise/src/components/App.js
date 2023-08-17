b68c594f5119fbd06f05d498f6f894e1c3f150d3
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2023-08-17 21:23:21 +0200

    finish excercise - useReducer bank account

--- 000 - exercise/src/components/App.js
+++ 000 - exercise/src/components/App.js
@@ -1,62 +1,130 @@
-import Accordion from "./Accordion";
-import TipCalc from "./TipCalc";
-import TextExpander from "./TextExpander";
-import CurrencyConverter from "./CurrencyConverter";
 import "../index.css";
-import Geolocation from "./Geolocation";
+import { useReducer } from "react";
+/*
+INSTRUCTIONS / CONSIDERATIONS:
 
-const faqs = [
-  {
-    title: "Where are these chairs assembled?",
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
-  },
-  {
-    title: "How long do I have to return my chair?",
-    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
-  },
-  {
-    title: "Do you ship to countries outside the EU?",
-    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
-  },
-];
+1. Let's implement a simple bank account! It's similar to the example that I used as an analogy to 
+explain how useReducer works, but it's simplified (we're not using account numbers here)
+
+2. Use a reducer to model the following state transitions: openAccount, deposit, withdraw, requestLoan, payLoan, closeAccount. 
+Use the `initialState` below to get started.
+
+3. All operations (expect for opening account) can only be performed if isActive is true. If it's not, just return the original 
+state object. You can check this right at the beginning of the reducer
+
+4. When the account is opened, isActive is set to true. There is also a minimum deposit amount of 500 to open an account 
+(which means that the balance will start at 500)
+
+5. Customer can only request a loan if there is no loan yet. If that condition is met, the requested amount will be registered 
+in the 'loan' state, and it will be added to the balance. If the condition is not met, just return the current state
+
+6. When the customer pays the loan, the opposite happens: the money is taken from the balance, and the 'loan' will get back to 0. 
+This can lead to negative balances, but that's no problem, because the customer can't close their account now (see next point)
+
+7. Customer can only close an account if there is no loan, AND if the balance is zero. If this condition is not met, just return 
+the state. If the condition is met, the account is deactivated and all money is withdrawn. The account basically gets back to the 
+initial state
+*/
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  isActive: false,
+};
+
+//openAccount, deposit, withdraw, requestLoan, payLoan, closeAccount.
+function reducer(state, action) {
+  if (!state.isActive && action.type !== "openAccount") return;
+  switch (action.type) {
+    case "openAccount":
+      return { ...state, isActive: true, balance: action.payload };
+    case "deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "withdraw":
+      return {
+        ...state,
+        balance:
+          state.balance - action.payload >= 0 ? state.balance - action.payload : state.balance,
+      };
+    case "requestLoan":
+      return {
+        ...state,
+        loan: state.loan === 0 ? action.payload : state.loan,
+        balance: state.loan === 0 ? state.balance + action.payload : state.balance,
+      };
+    case "payLoan":
+      return { ...state, loan: 0, balance: state.balance - action.payload };
+    case "closeAccount":
+      if (state.loan > 0 || state.balance > 0) return state;
+      return initialState;
+    default:
+      throw new Error("no action found");
+  }
+}
 
 export default function App() {
+  const [{ balance, loan, isActive }, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <>
-      <div>{/* <Accordion data={faqs} /> */}</div>
-      {/* <div>
-        <TipCalc />
-      </div> */}
+    <div className="App">
+      <h1>useReducer Bank Account</h1>
+      <p className="text-el">Balance: {balance}</p>
+      <p className="text-el">Loan: {loan}</p>
 
-      {/* <div>
-        <TextExpander>
-          Space travel is the ultimate adventure! Imagine soaring past the stars and exploring new
-          worlds. It's the stuff of dreams and science fiction, but believe it or not, space travel
-          is a real thing. Humans and robots are constantly venturing out into the cosmos to uncover
-          its secrets and push the boundaries of what's possible.
-        </TextExpander>
-
-        <TextExpander
-          collapsedNumWords={20}
-          expandButtonText="Show text"
-          collapseButtonText="Collapse text"
-          buttonColor="#ff6622"
+      <p>
+        <button
+          className="input-el"
+          onClick={() => dispatch({ type: "openAccount", payload: 500 })}
+          disabled={isActive}
         >
-          Space travel requires some seriously amazing technology and collaboration between
-          countries, private companies, and international space organizations. And while it's not
-          always easy (or cheap), the results are out of this world. Think about the first time
-          humans stepped foot on the moon or when rovers were sent to roam around on Mars.
-        </TextExpander>
-
-        <TextExpander expanded={true} className="box">
-          Space missions have given us incredible insights into our universe and have inspired
-          future generations to keep reaching for the stars. Space travel is a pretty cool thing to
-          think about. Who knows what we'll discover next!
-        </TextExpander>
-      </div> */}
-
-      {/* <CurrencyConverter /> */}
-      <Geolocation />
-    </>
+          Open account
+        </button>
+      </p>
+      <p>
+        <button
+          className="input-el"
+          onClick={() => dispatch({ type: "deposit", payload: 150 })}
+          disabled={!isActive}
+        >
+          Deposit 150
+        </button>
+      </p>
+      <p>
+        <button
+          className="input-el"
+          onClick={() => dispatch({ type: "withdraw", payload: 50 })}
+          disabled={!isActive || balance === 0}
+        >
+          Withdraw 50
+        </button>
+      </p>
+      <p>
+        <button
+          className="input-el"
+          onClick={() => dispatch({ type: "requestLoan", payload: 5000 })}
+          disabled={!isActive || loan === 5000}
+        >
+          Request a loan of 5000
+        </button>
+      </p>
+      <p>
+        <button
+          className="input-el"
+          onClick={() => dispatch({ type: "payLoan", payload: 5000 })}
+          disabled={!isActive || loan === 0 || balance < 5000}
+        >
+          Pay loan
+        </button>
+      </p>
+      <p>
+        <button
+          className="input-el"
+          onClick={() => dispatch({ type: "closeAccount" })}
+          disabled={!isActive || loan !== 0 || balance !== 0}
+        >
+          Close account
+        </button>
+      </p>
+    </div>
   );
 }
